2bba5315293a5c6228d004427d4d1a2b7572dff2
Estefanos <ekebebew@sfsu.edu>
2023-07-04 19:41:19 -0700

    added new features

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 **Description**
 
-- Convert SNPs into 2DCNN
+- Convert SNPs into 1DCNN
 
 **Dataset**
 
@@ -10,6 +10,10 @@
 
     - cip_ctx_ctz_gen_multi_data.csv contains the strains and their respective single nucleotide polymorphisms. Number of features: 60937
     
+        `` The data are encoded as follows: 0 = Null, 1 = A, 2 = C, 3 = G, 4 = T ``
+    
     - cip_ctx_ctz_gen_pheno.csv contains the drug resistance of each strains. These drugs are ciprofloxacin, cefotaxime, ceftazidime and gentamicin. Number of features: 4
+
+        `` The data are encoded as follows: 0 = Susceptible, 1 = Resistant ``  
  
 
--- preprocessing.py
+++ preprocessing.py
@@ -4,6 +4,10 @@ class Preprocessing:
     def __init__(self, file_path):
         self.df = pd.read_csv(file_path)
 
+    # return the dataframe
+    def get_dataframe(self):
+        print(self.df)
+
     # return the number of columns in the dataset
     def count_number_of_features(self):
         print(len(self.df.columns))
@@ -12,10 +16,31 @@ class Preprocessing:
     def count_number_of_rows(self):
         print(len(self.df.index))
 
-# the instance of the Preprocessing class
-# path needs to change to the location of the dataset of the working machine
-prep = Preprocessing('/Users/estefanosk/Desktop/SummerProject/Giessen_Dataset/cip_ctx_ctz_gen_multi_data.csv')
+    # count the number of rows with missing values where 0 stands for the missing value
+    def count_number_of_missing_values(self):
+        missing_values = 0
+        for index, row in self.df.iterrows():
+            if (row == 0).any():
+                missing_values += 1
+        print("Number of missing Snp's: ", missing_values)
 
-# Call the class methods using the instance
-prep.count_number_of_rows()
+    def get_strain_name_and_snps(self):
+        # Get the strain name from the first element in the first row
+        strain_name = self.df.iloc[0, 0]
+        print("Strain Name:", strain_name)
+
+        # Get the SNPs associated with the strain in the same column
+        snps = self.df[self.df.columns[0]].values
+        print("SNPs:")
+        print(snps)
+
+
+# the instance of the Preprocessing class
+# path needs to be changed to the location of the dataset of the working machine
+prep = Preprocessing('/Users/estefanosk/Desktop/SummerProject/snps-to-2dcnn-converter/Giessen_Dataset/cip_ctx_ctz_gen_multi_data.csv')
+
+# calling the methods
+prep.get_dataframe()
 prep.count_number_of_features()
+prep.count_number_of_rows()
+prep.get_strain_name_and_snps()
